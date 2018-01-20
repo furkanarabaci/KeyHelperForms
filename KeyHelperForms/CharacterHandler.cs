bec4c09b56f570bfc69448b7926578a749a830c6
Furkan Arabacı <furkanarabaci@hotmail.com>
2018-01-20 20:04:27 +0300

    This is the only class which will be used in MainForms.cs

--- KeyHelperForms/CharacterHandler.cs
+++ KeyHelperForms/CharacterHandler.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,41 @@ namespace KeyHelperForms
 {
     class CharacterHandler
     {
+        List<Character> characterList;
+        public CharacterHandler()
+        {
+            characterList = new List<Character>();
+        }
+        public void AddCharacter(Process characterProcess)
+        {
+            characterList.Add(new Character(characterProcess));
+        }
+        public void RemoveCharacter(Process characterProcess)
+        {
+            characterList.Remove(FindProcess(characterProcess.Id));
+        }
+        public void StartCharacterPress(int index)
+        {
+            characterList[index].StartPressing();
+        }
+        public void StopCharacterPress(int index)
+        {
+            characterList[index].StopPressing();
+        }
+        protected Character FindProcess(int processId)
+        {
+            foreach(Character currentChar in characterList)
+            {
+                if(currentChar.ProcessId == processId)
+                {
+                    return currentChar;
+                }
+            }
+            return null; //It usually wont reach here, just in case.
+        }
+        public void ResetCharacters()
+        {
+            characterList.Clear();
+        }
     }
 }
