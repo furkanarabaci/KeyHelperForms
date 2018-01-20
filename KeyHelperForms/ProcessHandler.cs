ab97fe5a10e750a8b26b38bf8c8b0ab061c182a9
Furkan Arabacı <furkanarabaci@hotmail.com>
2018-01-20 18:40:06 +0300

    Some methods have been divided

--- KeyHelperForms/ProcessHandler.cs
+++ KeyHelperForms/ProcessHandler.cs
@@ -20,22 +20,29 @@ namespace KeyHelperForms
         public static extern bool ReadProcessMemory(int hProcess,
         int lpBaseAddress, byte[] lpBuffer, int dwSize, ref int lpNumberOfBytesRead);
 
-        public string ReadAddress(Process process, int addressValue, Object dataType)
+        public string ReadStringAddress(Process process, int addressValue)
         {
             //Address value in hexadecimal. The variable address points can vary, so use it at your own risk.
             //dataType requires a value, you can pass anything as a parameter.
             IntPtr processHandle = OpenProcess(PROCESS_WM_READ, false, process.Id);
 
             int bytesRead = 0;
-            byte[] buffer = new byte[Variables.bufferSize];
+            byte[] buffer = new byte[Variables.stringBufferSize];
 
             ReadProcessMemory((int)processHandle, addressValue, buffer, buffer.Length, ref bytesRead);
-            if (dataType.GetType() == typeof(int))
-            {
-                return BitConverter.ToInt32(buffer, 0).ToString();
-            }
             return Encoding.UTF8.GetString(buffer); // Return string as default.
-       }
+        }
+        public int ReadIntAddress(Process process, int addressValue)
+        {
+            IntPtr processHandle = OpenProcess(PROCESS_WM_READ, false, process.Id);
+
+            int bytesRead = 0;
+            byte[] buffer = new byte[Variables.intBufferSize];
+
+            ReadProcessMemory((int)processHandle, addressValue, buffer, buffer.Length, ref bytesRead);
+
+            return BitConverter.ToInt32(buffer, 0);
+        }
         public List<Process> GetProcesses()
         {
             return Process.GetProcesses().ToList();
--- KeyHelperForms/Variables.cs
+++ KeyHelperForms/Variables.cs
@@ -10,21 +10,26 @@ namespace KeyHelperForms
     {
         public static string processName = "PVO_Client";
         public const UInt32 WM_KEYDOWN = 0x0100;
+        struct Keys
+        {
+            public const int VK_KEY_0 = 0x30;
+            public const int VK_KEY_1 = 0x31;
+            public const int VK_KEY_2 = 0x32;
+            public const int VK_KEY_3 = 0x33;
+            public const int VK_KEY_4 = 0x34;
+            public const int VK_KEY_5 = 0x35;
+            public const int VK_KEY_6 = 0x36;
+            public const int VK_KEY_7 = 0x37;
+            public const int VK_KEY_8 = 0x38;
+            public const int VK_KEY_9 = 0x39;
+        }
 
-        public const int VK_KEY_0 = 0x30;
-        public const int VK_KEY_1 = 0x31;
-        public const int VK_KEY_2 = 0x32;
-        public const int VK_KEY_3 = 0x33;
-        public const int VK_KEY_4 = 0x34;
-        public const int VK_KEY_5 = 0x35;
-        public const int VK_KEY_6 = 0x36;
-        public const int VK_KEY_7 = 0x37;
-        public const int VK_KEY_8 = 0x38;
-        public const int VK_KEY_9 = 0x39;
 
         public static int sleepTime = 1000;
-        public const int bufferSize = 24; //Buffer for various things. 
 
+        public const int intBufferSize = 24;
+        public const int stringBufferSize = 24;
+   
         public const int characterNameAddress = 0x010916BF;
         public const int hpAddress = 0x00E70DD4;
         public const int mpAddress = 0x00E70DD8;
@@ -35,16 +40,16 @@ namespace KeyHelperForms
         {
             return new List<int>
             {
-                VK_KEY_1,
-                VK_KEY_2,
-                VK_KEY_3,
-                VK_KEY_4,
-                VK_KEY_5,
-                VK_KEY_6,
-                VK_KEY_7,
-                VK_KEY_8,
-                VK_KEY_9,
-                VK_KEY_0
+                Keys.VK_KEY_1,
+                Keys.VK_KEY_2,
+                Keys.VK_KEY_3,
+                Keys.VK_KEY_4,
+                Keys.VK_KEY_5,
+                Keys.VK_KEY_6,
+                Keys.VK_KEY_7,
+                Keys.VK_KEY_8,
+                Keys.VK_KEY_9,
+                Keys.VK_KEY_0
             };
         }
     }
