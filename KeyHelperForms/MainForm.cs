84d98e993a3d31ee7d96499e24ef73635ed40b97
Furkan Arabacı <furkanarabaci@hotmail.com>
2018-01-21 17:32:14 +0300

    Tidying up code with regions and more methods

--- KeyHelperForms/MainForm.cs
+++ KeyHelperForms/MainForm.cs
@@ -25,30 +25,10 @@ namespace KeyHelperForms
         }
         private void MainForm_Load(object sender, EventArgs e)
         {
-            FillListView();
+            RefreshListView();
             DeactivateCheckBoxes(); //No selected items, so disabled.
         }
-        private void btnRefresh_Click(object sender, EventArgs e)
-        {
-            //TODO : This button is pointless, remove it and check for processes initially.
-            listView_Characters.Items.Clear(); //Also works as refresh.
-            FillListView();
-        }
-        private void ChangeCheckState(CheckBox currentCheckBox, int index)
-        {
-            if(selectedIndex == -1)
-            {
-                return; //Don't take an OutOfBoundException head on !
-            }
-            if (currentCheckBox.Checked)
-            {
-                characterHelper.Characters[selectedIndex].CheckState[index] = true;
-            }
-            else
-            {
-                characterHelper.Characters[selectedIndex].CheckState[index] = false;
-            }
-        }
+        #region CheckBoxes and submethods
         private void checkBox_key1_CheckedChanged(object sender, EventArgs e)
         {
             ChangeCheckState(checkBox_key1, 0);
@@ -97,25 +77,112 @@ namespace KeyHelperForms
         {
             ChangeCheckState(checkBox_key0, 9);
         }
-
-        private void btnOffset_Click(object sender, EventArgs e)
+        private void ChangeCheckState(CheckBox currentCheckBox, int index)
         {
+            if (selectedIndex == -1)
+            {
+                return; //Don't take an OutOfBoundException head on !
+            }
+            if (currentCheckBox.Checked)
+            {
+                characterHelper.Characters[selectedIndex].CheckState[index] = true;
+            }
+            else
+            {
+                characterHelper.Characters[selectedIndex].CheckState[index] = false;
+            }
+        }
+        private void ActivateCheckBoxes()
+        {
+            checkBox_key1.Enabled = true;
+            checkBox_key2.Enabled = true;
+            checkBox_key3.Enabled = true;
+            checkBox_key4.Enabled = true;
+            checkBox_key5.Enabled = true;
+            checkBox_key6.Enabled = true;
+            checkBox_key7.Enabled = true;
+            checkBox_key8.Enabled = true;
+            checkBox_key9.Enabled = true;
+            checkBox_key0.Enabled = true;
 
         }
-        public void FillListView()
+        private void DeactivateCheckBoxes()
+        {
+            checkBox_key1.Enabled = false;
+            checkBox_key2.Enabled = false;
+            checkBox_key3.Enabled = false;
+            checkBox_key4.Enabled = false;
+            checkBox_key5.Enabled = false;
+            checkBox_key6.Enabled = false;
+            checkBox_key7.Enabled = false;
+            checkBox_key8.Enabled = false;
+            checkBox_key9.Enabled = false;
+            checkBox_key0.Enabled = false;
+        }
+        private void ChangeCheckBoxes()
+        {
+            if (selectedIndex == -1)
+            {
+                checkBox_key1.Checked = false;
+                checkBox_key2.Checked = false;
+                checkBox_key3.Checked = false;
+                checkBox_key4.Checked = false;
+                checkBox_key5.Checked = false;
+                checkBox_key6.Checked = false;
+                checkBox_key7.Checked = false;
+                checkBox_key8.Checked = false;
+                checkBox_key9.Checked = false;
+                checkBox_key0.Checked = false;
+            }
+            else
+            {
+                checkBox_key1.Checked = characterHelper.Characters[selectedIndex].CheckState[0];
+                checkBox_key2.Checked = characterHelper.Characters[selectedIndex].CheckState[1];
+                checkBox_key3.Checked = characterHelper.Characters[selectedIndex].CheckState[2];
+                checkBox_key4.Checked = characterHelper.Characters[selectedIndex].CheckState[3];
+                checkBox_key5.Checked = characterHelper.Characters[selectedIndex].CheckState[4];
+                checkBox_key6.Checked = characterHelper.Characters[selectedIndex].CheckState[5];
+                checkBox_key7.Checked = characterHelper.Characters[selectedIndex].CheckState[6];
+                checkBox_key8.Checked = characterHelper.Characters[selectedIndex].CheckState[7];
+                checkBox_key9.Checked = characterHelper.Characters[selectedIndex].CheckState[8];
+                checkBox_key0.Checked = characterHelper.Characters[selectedIndex].CheckState[9];
+            }
+        }
+        #endregion
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            //TODO : This button is pointless, remove it and check for processes initially.
+            listView_Characters.Items.Clear(); //Also works as refresh.
+            RefreshListView();
+        }
+        #region ListBox and submethods
+        private void RefreshListView()
         {
             /* ListView structure -> 0 : charName | 1 : state */
+            //TODO : Here is too ugly, simplify it and move it elsewhere.
             List<Process> processList = ProcessHandler.GetRelativeProcesses();
             foreach (Process process in processList)
             {
-                characterHelper.AddCharacter(process);
-                int characterIndex = characterHelper.Characters.Count - 1; //We now our current process is last one added.
-                ListViewItem item = new ListViewItem();
-                //TODO : Here is too ugly, simplify it and move it elsewhere.
-                item.Text = characterHelper.Characters[characterIndex].CharacterName; //First column refers to text, not subitems.
-                item.SubItems.Add(Variables.Texts.stateStop); //Stopped at default.
-                listView_Characters.Items.Add(item);
+                AddNewRow(process);
             }
+        }
+        private void AddNewRow(Process processToBind)
+        {
+            //Adds a new row to listview and to the characterHandler object.
+            characterHelper.AddCharacter(processToBind);
+            int characterIndex = characterHelper.Characters.Count - 1; //We know that our current process is last one added.
+            ListViewItem item = new ListViewItem
+            {
+                Text = characterHelper.Characters[characterIndex].CharacterName, //First column refers to text, not subitems.
+            };
+            item.SubItems.Add(Variables.Texts.stateStop); //Stopped at default.
+            listView_Characters.Items.Add(item);
+        }
+        private void RemoveRow(Character characterToDelete) 
+        {
+            //Removes the given row from characterHandler object. No need to delete it from listview, we are refreshing anyways.
+            //Listview items are binded to characters, that's why i am taking the object to make things easier.
+            characterHelper.RemoveCharacter(characterToDelete);
         }
         private void listView_Characters_SelectedIndexChanged(object sender, EventArgs e)
         {
@@ -171,61 +238,6 @@ namespace KeyHelperForms
                 DeactivateCheckBoxes();  
             }
         }
-        private void ActivateCheckBoxes()
-        {
-            checkBox_key1.Enabled = true;
-            checkBox_key2.Enabled = true;
-            checkBox_key3.Enabled = true;
-            checkBox_key4.Enabled = true;
-            checkBox_key5.Enabled = true;
-            checkBox_key6.Enabled = true;
-            checkBox_key7.Enabled = true;
-            checkBox_key8.Enabled = true;
-            checkBox_key9.Enabled = true;
-            checkBox_key0.Enabled = true;
-            
-        }
-        private void DeactivateCheckBoxes()
-        {
-            checkBox_key1.Enabled = false;
-            checkBox_key2.Enabled = false;
-            checkBox_key3.Enabled = false;
-            checkBox_key4.Enabled = false;
-            checkBox_key5.Enabled = false;
-            checkBox_key6.Enabled = false;
-            checkBox_key7.Enabled = false;
-            checkBox_key8.Enabled = false;
-            checkBox_key9.Enabled = false;
-            checkBox_key0.Enabled = false;
-        }
-        private void ChangeCheckBoxes()
-        {
-            if (selectedIndex == -1)
-            {
-                checkBox_key1.Checked = false;
-                checkBox_key2.Checked = false;
-                checkBox_key3.Checked = false;
-                checkBox_key4.Checked = false;
-                checkBox_key5.Checked = false;
-                checkBox_key6.Checked = false;
-                checkBox_key7.Checked = false;
-                checkBox_key8.Checked = false;
-                checkBox_key9.Checked = false;
-                checkBox_key0.Checked = false;
-            }
-            else
-            {
-                checkBox_key1.Checked = characterHelper.Characters[selectedIndex].CheckState[0];
-                checkBox_key2.Checked = characterHelper.Characters[selectedIndex].CheckState[1];
-                checkBox_key3.Checked = characterHelper.Characters[selectedIndex].CheckState[2];
-                checkBox_key4.Checked = characterHelper.Characters[selectedIndex].CheckState[3];
-                checkBox_key5.Checked = characterHelper.Characters[selectedIndex].CheckState[4];
-                checkBox_key6.Checked = characterHelper.Characters[selectedIndex].CheckState[5];
-                checkBox_key7.Checked = characterHelper.Characters[selectedIndex].CheckState[6];
-                checkBox_key8.Checked = characterHelper.Characters[selectedIndex].CheckState[7];
-                checkBox_key9.Checked = characterHelper.Characters[selectedIndex].CheckState[8];
-                checkBox_key0.Checked = characterHelper.Characters[selectedIndex].CheckState[9];
-            }
-        }
+        #endregion
     }
 }
