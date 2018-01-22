e726b4fd80fd7384e99fb7c1b207fa4861b7dc69
Furkan Arabacı <furkanarabaci@hotmail.com>
2018-01-22 14:26:48 +0300

    Now it refreshes itself, no need to re-create object when user logs to different character.

--- KeyHelperForms/Character.cs
+++ KeyHelperForms/Character.cs
@@ -16,22 +16,36 @@ namespace KeyHelperForms
         public string CharacterName { get; private set; }
         public Character(Process paramProcess) : base(paramProcess)
         {
-            CheckState = Enumerable.Repeat(false, 10).ToList();
-            StartState = false;
             ClientProcess = paramProcess; //Gonna bind to the process
-            CharacterName = ProcessHandler.ReadStringAddress(paramProcess, Variables.Addresses.characterName);
+            RefreshProcess();
+            RefreshCharacterValues();
         }
         public void StartPressing()
         {
             StartState = true;
             ChangeChecks(CheckState); //KeyThreadArray function
             StartAll(); //KeyThreadArray function
-
         }
         public void StopPressing()
         {
             StartState = false;
             StopAll(); //KeyThreadArray function
+        }
+        public void RefreshCharacterValues()
+        {
+            string previousName = CharacterName;
+            CharacterName = ProcessHandler.ReadStringAddress(ClientProcess, Variables.Addresses.characterName).Replace(Variables.nullTerminator, String.Empty);
+            if(previousName != null && !previousName.Equals(CharacterName))
+            {
+                //Means the user restarted the client and logged in to a different client.
+                RefreshProcess();
+            }
+        }
+        private void RefreshProcess()
+        {
+            //Will occur when the user restarts and logs in to a different char, then uses keyhelper again. If so, reset everything to initial state.
+            CheckState = Enumerable.Repeat(false, 10).ToList();
+            StartState = false;
         }
     }
 }
