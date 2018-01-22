479c56977d377cfc92ee1cc1b767dd0ad24a521c
Furkan Arabacı <furkanarabaci@hotmail.com>
2018-01-22 19:07:16 +0300

    Thread wrapper that handles listbox refreshing

--- KeyHelperForms/ProcessThread.cs
+++ KeyHelperForms/ProcessThread.cs
@@ -2,11 +2,59 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace KeyHelperForms
 {
     class ProcessThread
     {
+        bool state = false; //This will be changed by Start() and Stop()
+        Thread procThread;
+        Action functionToInvoke;
+        public ProcessThread(Action function)
+        {
+            functionToInvoke = function;
+            RenewThread();
+            Start(); //You might want to remove this. It starts itself cuz we won't want this thread to always be working.
+        }
+        private void RenewThread()
+        {
+            procThread = new Thread(new ThreadStart(MainThread))
+            {
+                IsBackground = true,
+                Priority = ThreadPriority.BelowNormal //No need to overtire CPU
+                
+            };
+        }
+        private void MainThread()
+        {
+            while (true)
+            {
+                functionToInvoke();
+                Thread.Sleep(Variables.processThreadSleep);
+            }
+        }
+        public bool IsThreadWorking()
+        {
+            return procThread.IsAlive;
+        }
+        public void Start()
+        {
+            if (!state)
+            {
+                RenewThread();
+                procThread.Start();
+            }
+            state = true;
+        }
+        public void Stop()
+        {
+            if (state)
+            {
+                procThread.Abort();
+            }
+            state = false;
+        }
     }
 }
