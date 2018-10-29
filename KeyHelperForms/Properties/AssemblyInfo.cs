d0d47b9092530959d5cdeb03e82f2c287bf8eff2
Furkan Arabacı <furkanarabaci@hotmail.com>
2018-10-29 22:40:44 +0300

    Added icon, updated version to 1.1.0 and made stages for publishment

--- KeyHelperForms/Properties/AssemblyInfo.cs
+++ KeyHelperForms/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("KeyHelperForms")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A supported keyhelper for WarriorsWay™ Online")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("illegaldisease")]
 [assembly: AssemblyProduct("KeyHelperForms")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
